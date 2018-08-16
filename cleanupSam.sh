e545a1e78667cd5985d1932d278fda6c76aa6462
George Hamilton <george@onprem.com>
2018-08-16 15:23:56 -0500

    fix up cleanupSam.sh

--- cleanupSam.sh
+++ cleanupSam.sh
@@ -10,6 +10,12 @@ usage() {
 S3_BUCKET=$1
 STACK_NAME=$2
 
-aws s3 rb s3://${S3_BUCKET}
+echo -n "Remove bucket ${S3_BUCKET} (with force option)? (yes to continue)"
+read answer
+if [ "$answer" == "yes" ]; then
+  echo "Removing bucket ${S3_BUCKET} with force option."
+  aws s3 rb s3://${S3_BUCKET}/ --force
+fi
 
-aws cloudformation delete-stack --stack-name ${STACK_NAME}
+echo -n "Deleting stack named ${STACK_NAME}... ";
+aws cloudformation delete-stack --stack-name ${STACK_NAME} && echo "done"
